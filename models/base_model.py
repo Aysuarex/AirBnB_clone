00dd68ab560965556dc9d83c515d020425ca93ce
lexxyla <bamideleadefolaju@gmail.com>
2022-08-03 13:08:31 +0100

    Executable pyscript

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 """Defines the BaseModel"""
+import models
+
+
+class BaseModel:
+    """
+    Base model for the Airbnb Clone Project
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Basemodel Constructor"""
