09727a9e81aef264eb9c8dfee962f79ff26e4b3f
Aysuarex <aysuarex@gmail.com>
2022-08-05 03:52:55 +0100

    Added commands all and update

--- console.py
+++ console.py
@@ -61,9 +61,9 @@ class HBNBCommand(cmd.Cmd):
             return False
         if args[0] in class_list:
             if len(args) > 1:
-                key = args[0] + "." + args[1]
-                if key in models.storage.all():
-                    print(models.storage.all()[key])
+                instance = args[0] + "." + args[1]
+                if instance in models.storage.all():
+                    print(models.storage.all()[instance])
                 else:
                     print("** no instance found **")
                     return False
@@ -82,9 +82,10 @@ class HBNBCommand(cmd.Cmd):
             return False
         if args[0] in class_list:
             if len(args) > 1:
-                key = args[0] + "." + args[1]
-                if key in models.storage.all():
-                    print(models.storage.all()[key])
+                instance = args[0] + "." + args[1]
+                if instance in models.storage.all():
+                    models.storage.all().pop(instance)
+                    models.storage.save()
                 else:
                     print("** no instance found **")
                     return False
@@ -95,6 +96,63 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return False
 
+    def do_all(self, arg):
+            """Prints string representations of instances"""
+            args = shlex.split(arg)
+            a_list = []
+            if len(args) == 0:
+                for value in models.storage.all().values():
+                    a_list.append(str(value))
+                print("[", end="")
+                print(", ".join(a_list), end="")
+                print("]")
+            elif args[0] in class_list:
+                for instance in models.storage.all():
+                    if args[0] in instance:
+                        a_list.append(str(models.storage.all()[instance]))
+                print("[", end="")
+                print(", ".join(a_list), end="")
+                print("]")
+            else:
+                print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """Update an instance based on the class name, id, attribute & value"""
+        args = shlex.split(arg)
+        integers = ["number_rooms", "number_bathrooms", "max_guest",
+                    "price_by_night"]
+        floats = ["latitude", "longitude"]
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] in tom:
+            if len(args) > 1:
+                k = args[0] + "." + args[1]
+                if k in models.storage.all():
+                    if len(args) > 2:
+                        if len(args) > 3:
+                            if args[0] == "Place":
+                                if args[2] in integers:
+                                    try:
+                                        args[3] = int(args[3])
+                                    except:
+                                        args[3] = 0
+                                elif args[2] in floats:
+                                    try:
+                                        args[3] = float(args[3])
+                                    except:
+                                        args[3] = 0.0
+                            setattr(models.storage.all()[k], args[2], args[3])
+                            models.storage.all()[k].save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
 
 
