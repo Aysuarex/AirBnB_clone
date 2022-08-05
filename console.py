168609769ceb0f0c6ded5fc4c54c662830d128b4
Aysuarex <aysuarex@gmail.com>
2022-08-05 02:27:42 +0100

    Added do_show and do_destroy functions for console

---
+++ console.py
@@ -0,0 +1,100 @@
+#!/bin/bash/python3
+"""Entry point of the command interpreter"""
+
+import cmd
+import models
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import shlex
+
+
+class_list = {"BaseModel": BaseModel,
+              "City": City,
+              "State": State,
+              "Amenity": Amenity, 
+              "User": User, 
+              "Place": Place, 
+              "Review": Review
+              }
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class definition"""
+    prompt = '(hbnb)'
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+        
+    def do_EOF(self, arg):
+        """Exit console"""
+        return True
+
+    def emptyline(self):
+        """shouldnt execute anything"""
+        return False
+
+    def do_create(self, arg):
+        """Create a new instances of the class"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in class_list:
+            an_instance = class_list[args[0]]()
+        else:
+            print("** class doesn't exist **")
+            return False
+        print(an_instance.id)
+        an_instance.save()
+
+    def do_show(self, arg):
+        """Prints string representation of an instance"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in class_list:
+            if len(args) > 1:
+                instance = args[0] + "." + args[1]
+                if anstance in models.storage.all():
+                    print(models.storage.all()[instance])
+                else:
+                    print("** no instance found **")
+                    return False
+            else:
+                print("** instance id missing **")
+                return False
+        else:
+            print("** class doesn't exist **")
+            return False
+            
+    def do_destroy(self, arg):
+            """Deletes an instance based on the class and id"""
+            args = shlex.split(arg)
+            if len(args) == 0:
+                print("** class name missing **")
+            elif args[0] in tom:
+                if len(args) > 1:
+                    key = args[0] + "." + args[1]
+                    if key in models.storage.all():
+                        models.storage.all().pop(key)
+                        models.storage.save()
+                    else:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+            else:
+                print("** class doesn't exist **")
+
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
