e17586ec1b78a1693ccacfb0f09e2aac47e959e8
Aysuarex <aysuarex@gmail.com>
2022-08-06 22:40:38 +0100

    Updated file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,64 +1,51 @@
 #!/usr/bin/python3
-"""module of 'FileStorage' class"""
+"""Storage Class for HBnB.
 
-import os.path
+Attributes:
+    classes (TYPE): Description
+"""
 import json
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.city import City
 from models.place import Place
+from models.state import State
 from models.review import Review
 
-class_list = {"BaseModel": BaseModel,
-              "User": User,
-              "State": State,
-              "City": City,
-              "Amenity": Amenity,
-              "Place": Place,
-              "Review": Review
-              }
-white_list = []
-for key in class_list:
-    white_list.append(key)
+classes = {"BaseModel": BaseModel, "User": User, "Amenity": Amenity,
+           "City": City, "State": State, "Place": Place, "Review": Review}
 
 
-class FileStorage():
-    """Representation of a FileStorage"""
+class FileStorage:
+    """Return the dictionary __objects."""
 
     __file_path = "file.json"
     __objects = {}
 
-    def __init__(self):
-        """class constructor"""
-        pass
-
     def all(self):
-        """returns the dictionary '__objects'"""
+        """Return the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        """sets in '__objects' the 'obj' with key '<obj class name>.id'"""
-        key = obj.__class__.__name__+"."+obj.id
-        self.__objects.update({key: obj})
+        """Set in __objects the obj with key <obj class name>.id."""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """serializes '__objects' to the JSON file (__file_path)"""
-        dict = {}
+        """Serialize __objects to the JSON file (path: __file_path)."""
+        to_json = {}
         for key in self.__objects:
-            dict[key] = self.__objects[key].to_dict()
+            to_json[key] = self.__objects[key].to_dict()
         with open(self.__file_path, "w") as f:
-            json.dump(dict, f)
+            json.dump(to_json, f)
 
     def reload(self):
-        """deserializes the JSON file to '__objects'
-           (only if the JSON file (__file_path) exists, otherwise do nothing.
-           If the file doesn’t exist, no exception should be raised)"""
+        """Ddeserializes the JSON file to __objects."""
         try:
-            with open(self.__file_path, 'r') as f:
-                dict = json.load(f)
-            for i in dict:
-                self.__objects[i] = class_list[dict[i]["__class__"]](**dict[i])
-        except:
+            with open(self.__file_path, "r") as f:
+                for key, value in (json.load(f)).items():
+                    self.__objects[key] = classes[value["__class__"]](**value)
+        except FileNotFoundError:
             pass
